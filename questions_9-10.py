3363ba40d295d71984032ccf0bfcb8e8909a0b95
yan-s <110732997+Toppics@users.noreply.github.com>
2024-03-07 20:07:43 +0100

    Update questions_9-10.py

--- questions_9-10.py
+++ questions_9-10.py
@@ -8,13 +8,11 @@ Created on Wed Feb 28 23:00:25 2024
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-
 from math import sqrt
-
 
 # Les premiers nombres de la suite
 cache = {0: 0, 1: 1}
-
+#
 def fibonacci(n:int):
     if n in cache:  # Cas de base
         return cache[n]
@@ -22,31 +20,22 @@ def fibonacci(n:int):
     cache[n] = fibonacci(n - 1) + fibonacci(n - 2)  # Cas récursif
     return cache[n]
 
-suite = [fibonacci(n) for n in range(36+1)]
-# ou
-#print([fibonacci(n) for n in range(36+1)][1:])
+mois = 36
+#
+suite = [fibonacci(n) for n in range(mois+1)]
 
-
-
-#plt.xscale('log')
-#plt.yscale('log')
-plt.xlabel("X")
-plt.ylabel("Y")
+plt.xlim(1, mois)
+plt.xscale('log')
+plt.yscale('log')
 plt.legend(loc='upper left')
 
 #
-df = pd.DataFrame(cache.items())
-sns.lineplot(data=df, x=0, y=1, palette="tab10", label="couples", linewidth=1)
+df = pd.DataFrame(cache.items(), columns=['mois', 'couples'])
+sns.lineplot(data=df, x='mois', y='couples', palette="tab10", label="couples", linewidth=1)
 
-#%%
+#
+ρ = (1+sqrt(5))/2
+print(f'ρ ≃ {ρ} et le nombre d\'or approché vaut {suite[-1]/suite[-2]}')
 
-rho = (1+sqrt(5))/2
-print(f'rho : {rho}')
-print(f'Le nombre d\'or approché est {suite[-1]/suite[-2]}')
-
-k_rho = {}
-for k in cache.keys():
-    k_rho[k] = rho**k
-    
-df_k = pd.DataFrame(k_rho.items())
-sns.lineplot(data=df_k, x=0, y=1, palette="tab10", label="rho^k", linewidth=1)
+df_k = pd.DataFrame({k:ρ**k for k in cache.keys()}.items())
+sns.lineplot(data=df_k, x=0, y=1, palette="tab10", label="ρ^k", linewidth=1)
