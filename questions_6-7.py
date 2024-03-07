9a0bc1ce2e1343180b64e64c7b10bd857391f018
yan-s <110732997+Toppics@users.noreply.github.com>
2024-03-07 20:06:15 +0100

    Update questions_6-7.py

--- questions_6-7.py
+++ questions_6-7.py
@@ -6,26 +6,21 @@ Created on Tue Feb 27 12:03:00 2024
 
 majoration2 = abs(exp(z)-exp_approx(N, None, '((z**n)/factorial(n))')) <= ((abs(z)**(N+1))/(factorial(N+1)))*(1/(1-(abs(z)/(N+2))))
 """
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib import rcParams
 from cmath import exp
 from math import factorial
-
-e = lambda x: exp(x)
-e = e(1)
 
 def exp_approx(N:int, t:float, fct:str):
     somme = 0
     for n in range(0, N+1):
         somme += (eval(fct)**n)/(factorial(n))
     return somme
-  
+
+e = lambda x: exp(x); e = e(1)
 a = lambda N, z: abs(exp(z)-exp_approx(N, None, f'(({e}**n)/factorial(n))'))
 b = lambda N, z: ((abs(z)**(N+1))/(factorial(N+1)))*(1/(1-(abs(z)/(N+2))))
 
-N_max = 121 #question 6 ou 7
+N_max = 103
 N = [i for i in range(0, N_max+1)]
 
 dct = {}
@@ -34,20 +29,8 @@ for i in N:
 
 df = pd.DataFrame([(key, lst[0], lst[1]) for (key, lst) in dct.items()], columns=['N', 'a', 'b'])
 
-# %%
+# Question 6
+print(df.iloc[:21,1:])
 
-size = 10
-#
-if False:
-    size = 100
-    rcParams['figure.dpi'] = 300
-    rcParams['figure.figsize'] = (size, size)
-
-sns.lineplot(data=df, x='N', y='a', palette="tab10", label="a", linewidth=1)
-sns.lineplot(data=df, x='N', y='b', palette="tab10", label="b", linewidth=1)
-
-#plt.xlim(-1,1)
-#plt.ylim(-1,1)
-#plt.xscale('log')
-#plt.yscale('log')
-plt.legend(loc='upper right', fontsize=size)
+# Question 7
+print(df.iloc[-1:,1:])
