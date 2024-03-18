ee732b7fe84d19592c7a231ab52abb7f674bdd69
yan-s <110732997+Toppics@users.noreply.github.com>
2024-03-18 21:32:18 +0100

    Update LaTeX.py

--- LaTeX.py
+++ LaTeX.py
@@ -4,118 +4,119 @@ Created on Wed Mar  6 23:35:42 2024
 
 @author: yan-s
 """
-# %%
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from cmath import exp
+# %% En-tête commun à toutes les questions
+from cmath import exp, sin, cos
 from math import factorial, sqrt
 from matplotlib import rcParams
-
 from warnings import filterwarnings
-filterwarnings("ignore")
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#
-size = 10
-#
-if False:
-    size = 100
-    rcParams['figure.dpi'] = 300
-    rcParams['figure.figsize'] = (size, size)
+filterwarnings("ignore")    # Retirer les avertissements de la console
 
-plt.rcParams["legend.loc"] = "upper left"
-plt.rcParams["legend.fontsize"] = size
+rcParams['figure.dpi'] = 300    # Augmenter la résolution des figures
+plt.rcParams["legend.loc"] = "upper left"   # Titre en haut à gauche
 
-# Paramètre d'entrée
+# Paramètres d'entrée
 N = [1, 2, 5, 8]
 intervalle = (-4, 4)
-pas = 10e-2     # Correspond à 81 points
+PAS = 10e-2     # NotaBene: ici correspond à 81 points
 
-# Fonction approchée de exp
-def exp_approx(N:int, z:complex):
-    somme = 0
-    for n in range(0, N+1):
-        somme += (z**n)/(factorial(n))
-    return somme
+def exp_approx(N:int, z:complex)->complex:
+    """Somme de (z^n)/(n!), avec n=E([0,N])."""
+    return sum((z**n)/(factorial(n)) for n in range(0, N+1))
 
-# Fonction pour approximer func
-def exp_approx_func(N:int, t:float, fct:str):
-    somme = 0
-    for n in range(0, N+1):
-        somme += (eval(fct)**n)/(factorial(n))
-    return somme
+def exp_approx_func(N:int, t:float, fct:str)->float:
+    """Somme de ('func'^n)/(n!), avec n=E([0,N])."""
+    return sum((eval(fct)**n)/(factorial(n)) for n in range(0, N+1))
 
-# Fonction pour créer des t
-def sequence(debut, fin, pas=1):
+def sequence(debut:int|float, fin:int|float, pas:int|float=1)->list[float]:
+    """Créer une liste de 'n = (fin-début)/pas' points."""
     n = int(round((fin - debut)/float(pas)))
-    if n > 1: return([debut + pas*i for i in range(n+1)])
-    elif n == 1: return([debut])
-    else: return([])
+    if n > 1: return [debut + pas*i for i in range(n+1)]
+    elif n == 1: return [debut]
+    else: return []
 
-# Définie une suite de points espacé de pas dans l'intervalle
-sequence_de_t = sequence(intervalle[0], intervalle[1], pas)
+# Définie une liste de t à l'aide des paramètres d'entrée
+sequence_de_t = sequence(intervalle[0], intervalle[1], PAS)
 
 # %% Question 3
+FONCTION = 't'  # A rentrer dans exp_approx_func
 
-# On crée un DataFrame pour les x
-df_x = pd.DataFrame({key: exp(key) for key in sequence_de_t}.items(), columns=['x', 'y'])
-# On crée un DataFrame pour les N
-df_N = pd.DataFrame({key: exp(key) for key in N}.items(), columns=['x', 'y'])
-# On trace les différentes courbes
-sns.lineplot(data=df_x, x='x', y='y', palette="tab10", label="ref", linewidth=1)
-sns.lineplot(data=df_N, x='x', y='y', palette="tab10", label="approx", linewidth=1, marker='o')
+# On crée un jeu de données pour la fonction exp de référence
+df_ref = pd.DataFrame({key: exp(key) for key in sequence_de_t}.items(), columns=['x', 'y'])
+plt.plot(df_ref['x'],df_ref['y'], label="ref", linewidth=2, linestyle='dashed')  # On le trace
+
+for nb in N:    # Pour chaque nombre de N
+    N_vals = []
+    for t in sequence_de_t: # On crée une liste
+        N_vals.append([nb, t, exp_approx_func(nb, t, FONCTION)])
+    # On crée un de jeu de données à partir de la liste
+    df_N = pd.DataFrame(N_vals, columns=['t', 'x', 'y'])
+    plt.plot(df_N['x'],df_N['y'], label=f"{df_N['t'][0]}")  # On le trace
 
 # Différentes configurations pour rendre le graphe plus lisible
-plt.ylim(1)
-plt.xscale('log')
-plt.yscale('log')
+plt.grid(); plt.ylim(1)
+plt.legend(bbox_to_anchor=(1.05, 1), shadow=True)
+plt.show()
 
 # %% Question 4 ou 5
-
-if True:
+if False:
     # Question 4
-    fonction = '(t*1j)'    
+    FONCTION = '(t*1j)'
 else:
     # Question 5
-    fonction = '(t + t*1j)'
+    FONCTION = '(t + t*1j)'
 
-def tracer(df:pd.DataFrame, label:str):
-    sns.lineplot(data=df, x='x', y='y', palette="tab10", label=label, linewidth=1)
-    # Affiche le premier point & dernier point
+def tracer_avec_points(df:pd.DataFrame, label:str, **kwargs):
+    """Trace une courbe en plus du premier et dernier point."""
+    plt.plot(df['x'],df['y'], label=label, **kwargs)
+    # Trace le premier point & dernier point
     ppt, dpt = df.loc[1].values.flatten().tolist()[-2:], df.loc[-1:].values.flatten().tolist()[-2:]
     plt.plot(ppt[0], ppt[1], marker='o', color='black')
     plt.plot(dpt[0], dpt[1], marker='^', color='black')
-    
-#
-df_t = pd.DataFrame([[t,(tmp:=exp(eval(fonction))).real,tmp.imag] for t in sequence_de_t], columns=['t', 'x', 'y'])
-#    
-tracer(df_t, "ref")
+
+# On crée un jeu de données pour la fonction exp de référence
+t_vals = [[t,(tmp:=exp(eval(FONCTION))).real,tmp.imag] for t in sequence_de_t]
+df_ref = pd.DataFrame(t_vals, columns=['t', 'x', 'y'])
+tracer_avec_points(df_ref, "ref", linewidth=3, linestyle='dashed')  # On le trace
 
 #
-courbes_N = [pd.DataFrame([[element,(N_t:=exp_approx_func(element,t,fonction)).real,N_t.imag] for t in sequence_de_t], columns=['t', 'x', 'y']) for element in N] 
-#
-for df_N in courbes_N:
-    tracer(df_N, df_N['t'][0])
+for nb in N:    # Pour chaque nombre de N
+    N_vals = []
+    for t in sequence_de_t: # On crée une liste
+        N_t = exp_approx_func(nb, t, FONCTION)
+        N_vals.append([nb, N_t.real, N_t.imag])
+    # On crée un de jeu de données à partir de la liste
+    df_N = pd.DataFrame(N_vals, columns=['t', 'x', 'y'])
+    tracer_avec_points(df_N, f'{nb}')
 
-plt.grid()
-#plt.xscale('log')
+# Différentes configurations pour rendre le graphe plus lisible
+plt.grid(); plt.legend(bbox_to_anchor=(1.05, 1), shadow=True)
 plt.title('Premier point : •  Dernier point : ▲')
+plt.show()
 
 # %% Question 6 et 7
+def puissance_nombre(nombre:float)->float:
+    """De 1.0e-145, renvoie 145."""
+    nombre = str(nombre)
+    index_de_e = nombre.find('e')
+    return float(nombre[index_de_e+2:])
 
+z= 1
+decimal = 121
 e = lambda x: exp(x); e = e(1)
-a = lambda N, z: abs(exp(z)-exp_approx_func(N, None, f'(({e}**n)/factorial(n))'))
+a = lambda N, z: abs(exp(z)-exp_approx_func(N, None, f'{z}'))
 b = lambda N, z: ((abs(z)**(N+1))/(factorial(N+1)))*(1/(1-(abs(z)/(N+2))))
 
-N_max = 103
-N = [i for i in range(0, N_max+1)]
-
+N_MAX = 0
 dct = {}
-for i in N:
-    dct[i] = [a(i, e), b(i, e)]
+while (puissance_nombre(b(N_MAX, z))) <= decimal:
+    dct[N_MAX] = [a(N_MAX, z), b(N_MAX, z)]
+    N_MAX+=1
+dct[N_MAX] = [a(N_MAX, z), b(N_MAX, z)]
 
-df = pd.DataFrame([(key, lst[0], lst[1]) for (key, lst) in dct.items()], columns=['N', 'a', 'b'])
+df = pd.DataFrame([(key,lst[0],lst[1]) for (key, lst) in dct.items()], columns=['N', 'a', 'b'])
 
 # Question 6
 print(df.iloc[:21,1:])
@@ -124,45 +125,44 @@ print(df.iloc[:21,1:])
 print(df.iloc[-1:,1:])
 
 # %% Question 9 et 10
-
-# Les premiers nombres de la suite
-cache = {0: 0, 1: 1}
-#
-def fibonacci(n:int):
+cache = {0: 0, 1: 1}    # Les premiers nombres de la suite
+def fibonacci(n:int)->int:
+    """Calcul le n terme de la suite de Fibonacci."""
     if n in cache:  # Cas de base
         return cache[n]
     # On calcule et mémorise le nombre de la suite
     cache[n] = fibonacci(n - 1) + fibonacci(n - 2)  # Cas récursif
     return cache[n]
 
-mois = 36
-#
-suite = [fibonacci(n) for n in range(mois+1)]
+MOIS = 36   # Le nombre de n
+suite = [fibonacci(n) for n in range(MOIS+1)]   # Créer la suite de Fibonacci
 
-plt.xlim(1, mois)
-plt.xscale('log')
-plt.yscale('log')
-plt.legend(loc='upper left')
-
-#
+# On crée un jeu de données pour la suite k->uk
 df = pd.DataFrame(cache.items(), columns=['mois', 'couples'])
-sns.lineplot(data=df, x='mois', y='couples', palette="tab10", label="couples", linewidth=1)
+plt.plot(df['mois'],df['couples'], label='k->uk', linewidth=3, linestyle='dashed')
 
-#
-ρ = (1+sqrt(5))/2
+ρ = (1+sqrt(5))/2 # On pose ρ
 print(f'ρ ≃ {ρ} et le nombre d\'or approché vaut {suite[-1]/suite[-2]}')
 
-df_k = pd.DataFrame({k:ρ**k for k in cache.keys()}.items())
-sns.lineplot(data=df_k, x=0, y=1, palette="tab10", label="ρ^k", linewidth=1)
+# Sans corretion, -1 car u1=u2 : 1^0 = 1^1, avec le bon coeff
+ρ_FONCTION = ['ρ**k', 'ρ**(k-1)', '(1/sqrt(5))*ρ**k']
+for func in ρ_FONCTION: # Trace les différentes courbes
+    df_k = pd.DataFrame({k:eval(func) for k in cache.keys()}.items(), columns=['mois', 'couples'])
+    plt.plot(df_k['mois'],df_k['couples'], label=func)
+
+# Différentes configurations pour rendre le graphe plus lisible
+plt.legend(); plt.xlim(1, MOIS)
+plt.show()
 
 # %% Question 11 à 15
 
 #
-def tracer(func):        
+def tracer(func):
     def wrapper(*args, **kwargs):
         var = func(*args, **kwargs)
-        df = pd.DataFrame({k: v for k, v in enumerate(var)}.items(), columns=['x', 'y'])
-        sns.lineplot(data=df, x='x', y='y', palette="tab10", label=f'{func.__name__} y0={kwargs["y0"]}', linewidth=1)                
+        df = pd.DataFrame(dict(enumerate(var)).items(), columns=['x', 'y'])
+        plt.plot(df['x'],df['y'], label=f'{func.__name__} y0={kwargs["y0"]}')
+
     return wrapper
 
 
@@ -208,19 +208,19 @@ def lapins2Heun(r, Y, y0:int, h, N):
         solutions.append(y0*exp_approx(15, (r*n)))
     return solutions
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
     # Question 11
-    lapins(r= 0.5, y0=2, h=0, N=36)
+    lapins(r= 0.5, y0=2, h=1, N=36)
     # Question 12
-    lapinsEuler(r= 0.5, y0=2, h=0, N=36)
-    
+    lapinsEuler(r= 0.5, y0=2, h=1, N=36)
+
     for y0 in [2, 50, 100]:
         # Question 14
-        lapins2(r= 0.5, Y=50, y0=y0, h=0, N=36)
+        lapins2(r= 0.5, Y=50, y0=y0, h=1, N=36)
         # Question 15
-        lapinsEuler2(r= 0.5, Y=50, y0=y0, h=0, N=36)
-    
-    #
-    plt.yscale('log')
+        lapinsEuler2(r= 0.5, Y=50, y0=y0, h=1, N=36)
+
+    # Différentes configurations pour rendre le graphe plus lisible
+    plt.yscale('log'); plt.grid()
     plt.legend(bbox_to_anchor=(1.05, 1), shadow=True)
     plt.show()
