44adc54fa86c80a70f196f6a8fad65f16bfa10cf
yan-s <110732997+Toppics@users.noreply.github.com>
2024-05-04 10:05:45 +0200

    Update Euler_Heun.py

--- Euler_Heun.py
+++ Euler_Heun.py
@@ -7,18 +7,18 @@ Created on Tue Mar  5 14:35:48 2024
 from numpy import arange, exp
 import matplotlib.pyplot as plt
 
-def Euler(y0, h, f):
+def Euler(f, y0, h, N):
     """Approximation par la méthode d'Euler explicite."""
-    t = arange(0, 1 + h, h)
+    t = arange(0, 1 + N, h)
     y = list(range(len(t)))
     y[0] = y0
     for k in range(0, len(t) - 1):
         y[k + 1] = y[k] + h*f(t[k], y[k])
     return t, y
 
-def Heun(y0, h, f):
+def Heun(f, y0, h, N):
     """Approximation par la méthode d'Heun."""
-    t = arange(0, 1 + h, h)
+    t = arange(0, 1 + N, h)
     y = list(range(len(t)))
     y[0] = y0
     for k in range(1, len(t)):
@@ -29,14 +29,15 @@ def Heun(y0, h, f):
 if __name__ == '__main__':
     # Paramètres
     f = lambda t, s: exp(-t)
+    N = 36
     h = 1 # Pas
     y0 = -1 # Condition initiale
 
     # Plot Euler
-    t, s = Euler(y0, h, f)
-    plt.plot(t, s, linestyle='dotted', label='Approximate Euler')
+    t, s = Euler(f, y0, h, N)
+    plt.plot(t, s, linestyle='dashdot', label='Approximate Euler')
     # Plot Heun
-    t, y = Heun(y0, h, f)
+    t, y = Heun(f, y0, h, N)
     plt.plot(t, y, linestyle='dashed', label='Approximate Heun')
     # Plot Ref
     plt.plot(t, -exp(-t), linestyle='solid', label='Exact')
